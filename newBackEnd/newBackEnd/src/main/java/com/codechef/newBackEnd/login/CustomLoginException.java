d163b24f14e6256c15e07773378585419de05b51
AbduBekh <abderrahmenbelkhiria123@gmail.com>
2023-11-13 20:53:00 +0100

    New branch

--- newBackEnd/newBackEnd/src/main/java/com/codechef/newBackEnd/login/CustomLoginException.java
+++
@@ -1,7 +0,0 @@
-package com.codechef.newBackEnd.login;
-
-public class CustomLoginException extends RuntimeException {
-    public CustomLoginException(String message) {
-        super(message);
-    }
-}
--- newBackEnd/newBackEnd/src/main/java/com/codechef/newBackEnd/login/userLoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codechef.newBackEnd.login;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface userLoginRepository extends JpaRepository<userLogin, String> {
-    userLogin findByUSERNAMEAndPASSWORD(String username, String password);
-}
